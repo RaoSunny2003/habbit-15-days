7f9f39bc79a01b81b34a63ba4b3de4b68ca16a13
Rao Sunny <suaveraosunny@gmail.com>
2025-02-04 20:15:59 +0530

    task_tracker: Added Color to CLI

--- task_tracker/app.js
+++ task_tracker/app.js
@@ -23,6 +23,14 @@ const rli = readline.createInterface({
   output: process.stdout,
 });
 
+const colors = {
+  reset: "\x1b[0m",
+  green: "\x1b[32m",
+  red: "\x1b[31m",
+  yellow: "\x1b[33m",
+  cyan: "\x1b[36m",
+};
+
 function input(prompt) {
   return new Promise((resolve) => {
     rli.question(prompt, (uinput) => {
@@ -37,7 +45,7 @@ async function main() {
   let running = true;
 
   while (running) {
-    const choice = await input(`
+    const choice = await input(`${colors.cyan}
 Enter Number according to What you want to do?
     Task Tracker Menu:
         1. Add Task
@@ -45,52 +53,64 @@ Enter Number according to What you want to do?
         3. Delete Task
         4. List Task
         5. Exit
-Enter your choice: `);
+ ${colors.yellow}Enter your choice: ${colors.reset}`);
 
     switch (choice) {
       case "1":
-        const taskName = await input("Enter task name: ");
+        const taskName = await input(
+          `${colors.cyan}Enter task name: ${colors.reset}`
+        );
         tasks.push(taskName);
-        console.log("Task added!");
+        console.log(`${colors.green}Task added!${colors.reset}`);
         break;
       case "2":
-        const updateTaskId = await input("Enter task Id: ");
+        const updateTaskId = await input(
+          `${colors.cyan}Enter task Id: ${colors.reset}`
+        );
         if (updateTaskId > tasks.length) {
           console.log(typeof updateTaskId);
-          console.log("Wrong Input ID !");
+          console.log(`${colors.red}Wrong Input ID !${colors.reset}`);
         } else {
-          const newTask = await input("Enter updated Task: ");
+          const newTask = await input(
+            `${colors.cyan}Enter updated Task: ${colors.reset}`
+          );
           tasks[updateTaskId - 1] = newTask;
-          console.log("Task Updated Successfully...!");
+          console.log(
+            `${colors.green}Task Updated Successfully...!${colors.reset}`
+          );
         }
         break;
       case "3":
-        const deleteId = await input("Enter Task Id: ");
+        const deleteId = await input(
+          `${colors.cyan}Enter Task Id: ${colors.reset}`
+        );
         if (deleteId > tasks.length) {
-          console.log("Wrong Input ID !");
+          console.log(`${colors.red}Wrong Input ID !${colors.reset}`);
         } else {
           tasks.splice(deleteId - 1, 1);
 
-          console.log("Task Deleted!");
+          console.log(`${colors.green}Task Deleted!${colors.reset}`);
         }
         break;
       case "4":
         if (tasks.length === 0) {
-          console.log("No tasks added yet.");
+          console.log(`${colors.yellow}No tasks added yet.${colors.reset}`);
         } else {
-          console.log("Tasks:");
+          colors.yellow, console.log(`${colors.yellow}Tasks:${colors.reset}`);
           tasks.forEach((task, index) => {
-            console.log(`${index + 1}. ${task}`);
+            console.log(`${index + 1}.${colors.yellow} ${task}${colors.reset}`);
           });
         }
         break;
       case "5":
         running = false;
-        console.log("Exiting...");
+        console.log(`${colors.cyan}Exiting...${colors.reset}`);
         rli.close(); // Important: Close the readline interface
         break;
       default:
-        console.log("Invalid choice. Please try again.");
+        console.log(
+          `${colors.red}Invalid choice. Please try again.${colors.reset}`
+        );
     }
   }
 }
